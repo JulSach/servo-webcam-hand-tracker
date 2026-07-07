1f2434fb7a7022c492d0c4601db429eebd1a546c
JulSach <sachedinaj@gmail.com>
2026-07-07 19:34:48 -0400

    Feat: add MediaPipe tracking class

--- hand_tracker.py
+++ hand_tracker.py
@@ -10,4 +10,24 @@ class HandTracker:
         self.HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
         self.HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
         self.VisionRunningMode = mp.tasks.vision.RunningMode
-        
+
+        # Initialize HandLandmarker detector options, BaseOptions sets path for where model is
+        self.options = self.HandLandmarkerOptions(base_options = self.BaseOptions(model_asset_path = 'hand_landmarker.task'), running_mode = self.VisionRunningMode.LIVE_STREAM, result_callback = self.result_callback)
+
+        # Initialize HandLandmarker detector with previous options
+        self.landmarker = self.HandLandmarker.create_from_options(self.options)
+
+    def result_callback(self, result, output_image, timestamtp_ms):
+        # Print X Y and Z coords of each landmark
+        print('Hand Landmarker Result: {}'.format(result))
+
+    def inputFrame(self, frame):
+        # frame from OpenCv converted to MediaPipe Image object
+        img = mp.Image(mp.ImageFormat.SRGB, frame)
+        return img
+
+    def landmarkDetection(self, image, time):
+        self.landmarker.detect_async(image, time)
+    
+    def closeDetector(self):
+        self.landmarker.close()
