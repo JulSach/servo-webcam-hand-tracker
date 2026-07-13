1e12fef1c792ea414743450728b64b26ee2f75fa
JulSach <sachedinaj@gmail.com>
2026-07-12 22:37:18 -0400

    Feat: better design for coords printed in terminal

--- hand_tracker.py
+++ hand_tracker.py
@@ -12,17 +12,21 @@ class HandTracker:
         self.VisionRunningMode = mp.tasks.vision.RunningMode
 
         # Initialize HandLandmarker detector options, BaseOptions sets path for where model is
-        self.options = self.HandLandmarkerOptions(base_options = self.BaseOptions(model_asset_path = 'hand_landmarker.task'), running_mode = self.VisionRunningMode.LIVE_STREAM, result_callback = self.result_callback)
+        self.options = self.HandLandmarkerOptions(base_options = self.BaseOptions(model_asset_path = 'C:/Users/Julian Sachedina/OneDrive/Desktop/Project/hand_landmarker.task'), running_mode = self.VisionRunningMode.LIVE_STREAM, result_callback = self.result_callback)
 
         # Initialize HandLandmarker detector with previous options
         self.landmarker = self.HandLandmarker.create_from_options(self.options)
 
     def result_callback(self, result, output_image, timestamtp_ms):
         # Print X Y and Z coords of each landmark
-        print('Hand Landmarker Result: {}'.format(result))
+        print("---------Landmark Coords---------")
+        for hand_number, hands in enumerate(result.hand_landmarks): # Go through collection of hands
+            for landmark_number, landmarks in enumerate(hands): # Go through landmarks in each hand
+                print(f"Landmark {landmark_number}: x = {landmarks.x}, y = {landmarks.y}, z = {landmarks.z}") # Print each coord of each landmark
+        print("---------------------------------")
 
     def inputFrame(self, frame):
-        # Frame from OpenCv converted to MediaPipe Image object
+        # frame from OpenCv converted to MediaPipe Image object
         img = mp.Image(mp.ImageFormat.SRGB, frame)
         return img
 
