a3d504ad2bd0650c5d6011e82edb8f10aa0bb357
JulSach <300146244+JulSach@users.noreply.github.com>
2026-07-19 10:46:08 -0400

    feat: created points w/3 landmarks

--- angle_math.py
+++ angle_math.py
@@ -1,5 +1,8 @@
-import numpy
+import numpy as np
 
 class AngleMath:
-    def __init__(self):
-        pass
+    def fingerAngle(self, landmark_a, landmark_b, landmark_c):
+        # Create the points with the coords of 3 given landmarks
+        point_a = np.array([landmark_a.x, landmark_a.y])
+        point_b = np.array([landmark_b.x, landmark_b.y])
+        point_c = np.array([landmark_c.x, landmark_c.y])
